71b536c5cf04060555392ff0c9dd155ea315c3b3
LeeTrent <lee.trent.1@gmail.com>
2025-03-21 11:05:53 -0500

    Added Product domain model

---
+++ eshop-distributed/Catalog/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; } 
+        public required string Description { get; set; }
+        public decimal Price { get; set; } = decimal.Zero;
+        public required string ImageUrl { get; set; }
+
+    }
+}
