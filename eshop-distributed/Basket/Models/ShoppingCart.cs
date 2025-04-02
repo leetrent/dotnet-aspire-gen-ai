e9fde8bd1e28b3384063d44a44512febd15b4764
LeeTrent <lee.trent.1@gmail.com>
2025-04-02 10:15:15 -0500

    Added basket model classes

---
+++ eshop-distributed/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.Models
+{
+    public class ShoppingCart
+    {
+        public required string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = [];
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+    }
+}
---
+++ eshop-distributed/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Models
+{
+    public class ShoppingCartItem
+    {
+        public required int ProductId { get; set; } = default;
+        public required int Quantity { get; set; } = default;
+        public required decimal Price { get; set; } = default;
+        public required string? ProductName { get; set; } = default;
+        public string? Color { get; set; } = default;
+
+    }
+}
