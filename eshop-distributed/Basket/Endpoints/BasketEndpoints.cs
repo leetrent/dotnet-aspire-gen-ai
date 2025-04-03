cba15a2b747fb69344acd6576de5832bf1be3841
LeeTrent <lee.trent.1@gmail.com>
2025-04-03 16:20:23 -0500

    Added Basket endpoints

---
+++ eshop-distributed/Basket/Endpoints/BasketEndpoints.cs
@@ -0,0 +1,48 @@
+﻿namespace Basket.Endpoints
+{
+    public static class BasketEndpoints
+    {
+        public static void MapBasketEndpoints(this IEndpointRouteBuilder builder)
+        {
+            var group = builder.MapGroup("basket");
+
+            ////////////////////////////////////////////////////////////////////////////////////
+            // GET SHOPPING CART FOR USERNAME   
+            ////////////////////////////////////////////////////////////////////////////////////
+            group.MapGet("/{userName}", async (string userName, BasketService basketService) =>
+            {
+                var shoppingCart = await basketService.GetBasket(userName);
+                if (shoppingCart is null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(shoppingCart);
+            })
+            .WithName("GetBasket")
+            .Produces<ShoppingCart>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status404NotFound);
+
+            ////////////////////////////////////////////////////////////////////////////////////
+            // CREATE OR UPDATE SHOPPING CART (UPSERT)
+            ////////////////////////////////////////////////////////////////////////////////////
+            group.MapPost("/", async (ShoppingCart shoppingCart, BasketService basketService) =>
+            {
+                await basketService.UpdateBasket(shoppingCart);
+                return Results.Created("GetBasket", shoppingCart);
+            })
+            .WithName("UpdateBasket")
+            .Produces<ShoppingCart>(StatusCodes.Status201Created);
+
+            ////////////////////////////////////////////////////////////////////////////////////
+            // DELETE SHOPPING CART(UPSERT)
+            ////////////////////////////////////////////////////////////////////////////////////
+            group.MapDelete("/{userName}", async (string userName, BasketService basketService) =>
+            {
+                await basketService.DeleteBasket(userName);
+                return Results.NoContent();
+            })
+            .WithName("DeleteBasket")
+            .Produces(StatusCodes.Status204NoContent);
+        }
+    }
+}
