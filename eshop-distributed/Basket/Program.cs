94766694b45b125a9a1e49c5b3d0cd9e89b3c2e3
LeeTrent <lee.trent.1@gmail.com>
2025-04-03 16:29:42 -0500

    Registered basket endpoints in service class

--- eshop-distributed/Basket/Program.cs
+++ eshop-distributed/Basket/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Connections.Features;
+//using Microsoft.AspNetCore.Connections.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,32 +9,8 @@ builder.Services.AddScoped<BasketService>();
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
-
-// Configure the HTTP request pipeline.
+app.MapBasketEndpoints();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
