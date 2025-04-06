bdb509bd5394fe411089602aea2a10a53c236abf
LeeTrent <lee.trent.1@gmail.com>
2025-04-06 10:57:05 -0500

    Registered CatalogApiClient in AppHost's Project.cs

--- eshop-distributed/Basket/Program.cs
+++ eshop-distributed/Basket/Program.cs
@@ -6,6 +6,11 @@ builder.AddServiceDefaults();
 builder.AddRedisDistributedCache(connectionName: "cache"); // ConnectionStrings__cache
 builder.Services.AddScoped<BasketService>();
 
+builder.Services.AddHttpClient<CatalogApiClient>(client =>
+{
+    client.BaseAddress = new("https+http://catalog");
+});
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
