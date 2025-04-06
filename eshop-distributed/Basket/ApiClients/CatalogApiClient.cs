26925622c66a71a3ec5b229a1b98c51297604db1
LeeTrent <lee.trent.1@gmail.com>
2025-04-06 10:35:39 -0500

    Added CatalogApiClient for the purpose of using the Basket service endpoints

---
+++ eshop-distributed/Basket/ApiClients/CatalogApiClient.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Models;
+
+namespace Basket.ApiClients
+{
+    public class CatalogApiClient(HttpClient httpClient)
+    {
+        public async Task<Product?> GetProductById(int id)
+        {
+            return await httpClient.GetFromJsonAsync<Product>($"/catalog/{id}");
+         
+        }
+    }
+}
